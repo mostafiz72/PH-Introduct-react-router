1c2db7307bf3ea0758cb265451d70969d5c77cc0
Hera <mostafizhera@gmail.com>
2024-10-30 06:10:45 +0600

    react router project completed

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className=' text-center text-red-500 text-3xl font-semibold'>This is App page here now</div>
+  )
+}
